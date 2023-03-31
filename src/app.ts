15c9243304cced766c781df756cf59906f5eebd8
Arun P <kuttyarun1066@gmail.com>
2023-03-31 06:10:44 +0530

    Replaced structure

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import { bootstrapMicroframework } from "microframework";
+import { ioLoader } from "./Loaders/IoLoader";
+import { mongooseLoader } from "./Loaders/MongooseLoader";
+import { expressLoader } from "./Loaders/ExpressLoader";
+import { homeLoader } from "./Loaders/HomeLoader";
+bootstrapMicroframework({
+    loaders:[
+        ioLoader,
+        mongooseLoader,
+        expressLoader,
+        homeLoader
+    ]
+}).then(()=>{console.log('app running......')}).catch((err) => {console.log(err)});
