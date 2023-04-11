fa4ca13f49c021a4cb13ccdd68a1a9e868d1da0b
Arun P <kuttyarun1066@gmail.com>
2023-04-11 08:53:26 +0530

    create edit and detail api for admin user controller

---
+++ src/api/Controllers/request/UpdateUserRequest.ts
@@ -0,0 +1,11 @@
+export class UpdateUserRequest {
+    public firstName: string;
+    public lastName: string;
+    public address1: string;
+    public address2: string;
+    public city: string;
+    public state: string;
+    public country: string;
+    public mobileNumber: string;
+ }
+ 
