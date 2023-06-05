87d2cedb13a364c940cacf9ec0d155ecd3bc49dc
Arun P <kuttyarun1066@gmail.com>
2023-06-05 06:42:48 +0530

    Add swagger loader

--- Loaders/HomeLoader.ts
+++ Loaders/HomeLoader.ts
@@ -1,14 +1,13 @@
 import { MicroframeworkLoader, MicroframeworkSettings } from 'microframework';
 import {express} from 'express';
+import path = require('path');
 
 export const homeLoader: MicroframeworkLoader = async(settings: MicroframeworkSettings) => {
     console.log('home.......');
     
     const  expressApp = settings.getData('express_app');
+    const directory = path.join(__dirname, '../', 'views', 'arundhika');
     expressApp.get('/api', (req: express.request, res: express.response) => {
-        return res.json({
-            name: 'mongoose',
-            ersinon: '0.0.1'
-        })
+        return res.render(directory)
     })
 }
